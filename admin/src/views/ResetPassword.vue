fafa61308ad7e24c7864347b1ce2e5ed4fcabff3
haikaljs <haikaljavascript@gmail.com>
2024-07-23 01:32:18 +0800

    Create layout for guest users

--- admin/src/views/ResetPassword.vue
+++ admin/src/views/ResetPassword.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import GuestLayout from "../components/GuestLayout.vue";
+</script>
 
 <!--
   This example requires some changes to your config:
@@ -23,22 +25,7 @@
     <body class="h-full">
     ```
   -->
-    <div
-        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-    >
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-                class="mx-auto h-10 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-            />
-            <h2
-                class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-            >
-                Set new password
-            </h2>
-        </div>
-
+    <GuestLayout title="Set new password">
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="#" method="POST">
                 <div>
@@ -99,7 +86,7 @@
                 >
             </p>
         </div>
-    </div>
+    </GuestLayout>
 </template>
 
 <style scoped></style>
