c2f1424d4c76421893089d9d2e4a9725a4449b6f
haikaljs <haikaljavascript@gmail.com>
2024-07-23 02:10:14 +0800

    Create admin panel sidebar

--- admin/src/components/AppLayout.vue
+++ admin/src/components/AppLayout.vue
@@ -1,16 +1,19 @@
 <script setup>
+import Sidebar from "./Sidebar.vue";
 const { title } = defineProps({
     title: String,
 });
 </script>
 
 <template>
-    <div
-        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-    >
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">Part of layout</div>
-
-        <router-view></router-view>
+    <div class="min-h-full flex">
+        <Sidebar />
+        <div class="flex-1">
+            <header class="h-8 shadow bg-white">Header</header>
+            <main>
+                <router-view></router-view>
+            </main>
+        </div>
     </div>
 </template>
 
---
+++ admin/src/components/Sidebar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import {
+    HomeIcon,
+    UsersIcon,
+    ViewColumnsIcon,
+    ChartBarIcon,
+} from "@heroicons/vue/24/solid";
+</script>
+
+<template>
+    <div class="w-[200px] bg-pink-700 text-white py-4 px-2">
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center py-1 px-3 rounded transition-colors hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2"><HomeIcon class="size-6" /></span>
+            <span>Dashboard</span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center py-1 px-3 rounded transition-colors hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2"><ViewColumnsIcon class="size-6" /></span>
+            <span>Products</span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center py-1 px-3 rounded transition-colors hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2"><UsersIcon class="size-6" /></span>
+            <span>Users</span>
+        </router-link>
+        <router-link
+            :to="{ name: 'app.dashboard' }"
+            class="flex items-center py-1 px-3 rounded transition-colors hover:bg-black/30 mb-2"
+        >
+            <span class="mr-2"><ChartBarIcon class="size-6" /></span>
+            <span>Reports</span>
+        </router-link>
+    </div>
+</template>
+
+<style scoped></style>
