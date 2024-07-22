ca26c5e3acfbf7195aacc240b5c7f3de250dbce4
haikaljs <haikaljavascript@gmail.com>
2024-07-23 00:18:24 +0800

    Install vuex and create store

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import store from "./store";
+
+const test = computed(() => store.state.test);
+</script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h1 class="text-3xl font-bold">{{ test }}</h1>
 </template>
 
 <style scoped></style>
+import { computed } from 'vue';
