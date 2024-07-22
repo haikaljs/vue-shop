4cf720f4ba5bb947dea77b293044dfa100c7d407
haikaljs <haikaljavascript@gmail.com>
2024-07-23 01:11:43 +0800

    Create request password reset and reset password pages

--- admin/src/views/RequestPassword.vue
+++ admin/src/views/RequestPassword.vue
@@ -35,7 +35,7 @@
             <h2
                 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
             >
-                Sign in to your account
+                Request new password
             </h2>
         </div>
 
@@ -60,33 +60,6 @@
                 </div>
 
                 <div>
-                    <div class="flex items-center justify-between">
-                        <label
-                            for="password"
-                            class="block text-sm font-medium leading-6 text-gray-900"
-                            >Password</label
-                        >
-                        <div class="text-sm">
-                            <router-link
-                                :to="{ name: 'requestPassword' }"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >Forgot password?</router-link
-                            >
-                        </div>
-                    </div>
-                    <div class="mt-2">
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autocomplete="current-password"
-                            required=""
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-
-                <div>
                     <button
                         type="submit"
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -97,12 +70,12 @@
             </form>
 
             <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
+                Remember password?
                 {{ " " }}
-                <a
-                    href="#"
+                <router-link
+                    :to="{ name: 'login' }"
                     class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                    >Start a 14 day free trial</a
+                    >Go back to login</router-link
                 >
             </p>
         </div>
--- admin/src/views/ResetPassword.vue
+++ admin/src/views/ResetPassword.vue
@@ -35,24 +35,25 @@
             <h2
                 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
             >
-                Sign in to your account
+                Set new password
             </h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="#" method="POST">
                 <div>
-                    <label
-                        for="email"
-                        class="block text-sm font-medium leading-6 text-gray-900"
-                        >Email address</label
-                    >
+                    <div class="flex items-center justify-between">
+                        <label
+                            for="new-password"
+                            class="block text-sm font-medium leading-6 text-gray-900"
+                            >New Password</label
+                        >
+                    </div>
                     <div class="mt-2">
                         <input
-                            id="email"
-                            name="email"
-                            type="email"
-                            autocomplete="email"
+                            id="new-password"
+                            name="password"
+                            type="password"
                             required=""
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
@@ -62,24 +63,16 @@
                 <div>
                     <div class="flex items-center justify-between">
                         <label
-                            for="password"
+                            for="password-repeat"
                             class="block text-sm font-medium leading-6 text-gray-900"
-                            >Password</label
+                            >Repeat Password</label
                         >
-                        <div class="text-sm">
-                            <router-link
-                                :to="{ name: 'requestPassword' }"
-                                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >Forgot password?</router-link
-                            >
-                        </div>
                     </div>
                     <div class="mt-2">
                         <input
-                            id="password"
-                            name="password"
+                            id="password-repeat"
+                            name="password_repeat"
                             type="password"
-                            autocomplete="current-password"
                             required=""
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         />
@@ -91,18 +84,18 @@
                         type="submit"
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        Sign in
+                        Submit
                     </button>
                 </div>
             </form>
 
             <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
+                Remember password?
                 {{ " " }}
-                <a
-                    href="#"
+                <router-link
+                    :to="{ name: 'login' }"
                     class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                    >Start a 14 day free trial</a
+                    >Go back to login</router-link
                 >
             </p>
         </div>
