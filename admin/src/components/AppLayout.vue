b190915fa7b6621f843c5799c83d12226886dca9
haikaljs <haikaljavascript@gmail.com>
2024-07-23 01:40:26 +0800

    Create empty layout for authorized users

---
+++ admin/src/components/AppLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { title } = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div
+        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+    >
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">Part of layout</div>
+
+        <router-view></router-view>
+    </div>
+</template>
+
+<style scoped></style>
