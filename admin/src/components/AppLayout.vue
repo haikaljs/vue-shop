e37a33a7561cf549f6f8b43f875b05e6d90abe82
haikaljs <haikaljavascript@gmail.com>
2024-07-23 17:51:22 +0800

    Create admin panel navbar

--- admin/src/components/AppLayout.vue
+++ admin/src/components/AppLayout.vue
@@ -1,20 +1,24 @@
 <script setup>
 import Sidebar from "./Sidebar.vue";
+import TopHeader from "./TopHeader.vue";
 const { title } = defineProps({
     title: String,
 });
 </script>
 
 <template>
-    <div class="min-h-full flex">
+    <div class="min-h-full flex bg-gray-100">
         <Sidebar />
         <div class="flex-1">
-            <header class="h-8 shadow bg-white">Header</header>
-            <main>
-                <router-view></router-view>
+            <TopHeader />
+            <main class="p-6">
+                <div class="rounded p-4 bg-white">
+                    <router-view></router-view>
+                </div>
             </main>
         </div>
     </div>
 </template>
 
 <style scoped></style>
+import TopHeaderVue from "./TopHeader.vue";
---
+++ admin/src/components/TopHeader.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import {
+    ChevronDownIcon,
+    UserIcon,
+    ArrowRightEndOnRectangleIcon,
+} from "@heroicons/vue/20/solid";
+</script>
+
+<template>
+    <header class="h-14 shadow bg-white flex justify-between items-center">
+        <button class="p-4 text-gray-700">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6"
+            >
+                <path
+                    fill-rule="evenodd"
+                    d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </button>
+
+        <div class="px-4">
+            <Menu as="div" class="relative inline-block text-left">
+                <div>
+                    <MenuButton class="flex items-center">
+                        <img
+                            src="https://randomuser.me/api/portraits/men/60.jpg"
+                            alt=""
+                            class="rounded-full w-8 mr-2"
+                        />
+                        <small>Haikal Rozhan</small>
+                        <ChevronDownIcon
+                            class="h-5 w-5 text-violet-200 hover:text-violet-100"
+                            aria-hidden="true"
+                        />
+                    </MenuButton>
+                </div>
+
+                <transition
+                    enter-active-class="transition duration-100 ease-out"
+                    enter-from-class="transform scale-95 opacity-0"
+                    enter-to-class="transform scale-100 opacity-100"
+                    leave-active-class="transition duration-75 ease-in"
+                    leave-from-class="transform scale-100 opacity-100"
+                    leave-to-class="transform scale-95 opacity-0"
+                >
+                    <MenuItems
+                        class="absolute right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+                    >
+                        <div class="px-1 py-1">
+                            <MenuItem v-slot="{ active }">
+                                <button
+                                    :class="[
+                                        active
+                                            ? 'bg-pink-500 text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                >
+                                    <UserIcon
+                                        :active="active"
+                                        class="mr-2 h-5 w-5 text-pink-400"
+                                        aria-hidden="true"
+                                    />
+                                    Profile
+                                </button>
+                            </MenuItem>
+                            <MenuItem v-slot="{ active }">
+                                <button
+                                    :class="[
+                                        active
+                                            ? 'bg-pink-500 text-white'
+                                            : 'text-gray-900',
+                                        'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                                    ]"
+                                >
+                                    <ArrowRightEndOnRectangleIcon
+                                        :active="active"
+                                        class="mr-2 h-5 w-5 text-pink-400"
+                                        aria-hidden="true"
+                                    />
+                                    Logout
+                                </button>
+                            </MenuItem>
+                        </div>
+                    </MenuItems>
+                </transition>
+            </Menu>
+        </div>
+    </header>
+</template>
+
+<style scoped></style>
