02cbd962fada1726b7ae10506e742b904119ebd9
haikaljs <haikaljavascript@gmail.com>
2024-07-23 18:25:24 +0800

    Make layout responsive

--- admin/src/components/AppLayout.vue
+++ admin/src/components/AppLayout.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
 import Sidebar from "./Sidebar.vue";
-import TopHeader from "./TopHeader.vue";
+import Navbar from "./Navbar.vue";
 const { title } = defineProps({
     title: String,
 });
@@ -11,13 +11,32 @@ const sidebarOpened = ref(true);
 function toggleSidebar() {
     sidebarOpened.value = !sidebarOpened.value;
 }
+
+onMounted(() => {
+    handleSidebarOpened();
+    window.addEventListener("resize", handleSidebarOpened);
+});
+
+onUnmounted(() => {
+    window.removeEventListener("resize", handleSidebarOpened);
+});
+
+function handleSidebarOpened() {
+    if (window.outerWidth <= 768) {
+        sidebarOpened.value = false;
+    } else {
+        sidebarOpened.value = true;
+    }
+    // shorthand
+    // sidebarOpened.value = window.outerWidth > 768
+}
 </script>
 
 <template>
     <div class="min-h-full flex bg-gray-100">
         <Sidebar :class="{ '-ml-[200px]': !sidebarOpened }" />
         <div class="flex-1">
-            <TopHeader @toggle-sidebar="toggleSidebar" />
+            <Navbar @toggle-sidebar="toggleSidebar" />
             <main class="p-6">
                 <div class="rounded p-4 bg-white">
                     <router-view></router-view>
